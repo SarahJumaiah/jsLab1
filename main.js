cec04d0a41f1f626bfa7618cebac4056783ec89f
sarahjumaiah <sarahjumaiah@gmail.com>
2024-09-08 12:02:28 +0300

    done

---
+++ main.js
@@ -0,0 +1,43 @@
+//1
+let width = 2, height = 4
+console.log("The area of the rectangle is:", 2*(width+height) )
+
+//2 
+let temp = 40
+console.log("40°C is", (40 * 9/5) + 32,"°f")
+
+let f = 104
+console.log("104°f is", (f - 32) * 5/9,"°c")
+
+//3
+let hour = 2
+let second = hour * 3600
+console.log("2 hours = " , second,"Seconds")
+
+//4
+let name = "sarah";
+let lastname = " jumaiah";
+console.log(name+lastname);
+
+//5
+var bill = 10.25+3.99+7.15
+var tip = bill * 0.15
+var total = bill + tip 
+console.log(total);
+
+//6
+var adjective1 = "amazing";
+var adjective2 = "fun";
+var adjective3 = "entertaining";
+var madLib = `The Intro to JavaScript course is, ${adjective1}. James and Julia are so ${adjective2}. I cannot wait to work through the rest of this ${adjective3} content!`
+
+console.log(madLib);
+
+//7
+let firstname1 = "Sarah"
+let interest = "Develop websites"
+let hobby = "Netflix"
+
+let awesomeMessage = `Hi, my name is ${firstname1}. I love ${hobby}. In my spare time, I like to ${interest}`
+
+
